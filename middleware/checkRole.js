9036f2ce20df8e6c8b483334a4087e08eb08b7f7
Maxim-Chaliy <maximastratenko@yandex.ru>
2025-05-21 22:30:01 +0700

    21.05.2025

--- middleware/checkRole.js
+++ middleware/checkRole.js
@@ -1,9 +1,9 @@
-const checkRole = (roles) => (req, res, next) => {
-    const userRole = req.user.role; // Роль пользователя из токена
-    if (!roles.includes(userRole)) {
-        return res.status(403).send('Access denied');
-    }
-    next();
-};
+// const checkRole = (roles) => (req, res, next) => {
+//     const userRole = req.user.role; // Роль пользователя из токена
+//     if (!roles.includes(userRole)) {
+//         return res.status(403).send('Access denied');
+//     }
+//     next();
+// };
 
-module.exports = checkRole;
+// module.exports = checkRole;
